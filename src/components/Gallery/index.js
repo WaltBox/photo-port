2b74573b1bc8798eb2d2f99b8e95794471386568
Walt Boxwell <waltboxwell@gmail.com>
2022-06-17 10:34:22 -0500

    added testing

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-<Gallery currentCategory={currentCategory}></Gallery>
+
 function Gallery({ currentCategory }) {
   const { name, description } = currentCategory;
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
       <p>{description}</p>
-      <PhotoList  />
+      <PhotoList category={currentCategory.name}/>
     </section>
   );
 }
